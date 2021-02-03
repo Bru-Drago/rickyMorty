054f50a6d4270219c01b3563488548a3a6e8385d
Bruna Drago <bruna_drago@hotmail.com>
2021-02-02 21:02:38 -0300

    RMApp - recomeçando

--- Ricky&Morty/SupportFiles/Source/ViewController.swift
+++ Ricky&Morty/SupportFiles/Source/ViewController.swift
@@ -10,25 +10,26 @@ import Alamofire
 
 class ViewController: UIViewController {
     
-    @IBOutlet weak var textField: UITextField!
-    
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
+
         
+
         
-        let request =  AF.request("https://rickandmortyapi.com/api/character", method: .get)
-        request.responseJSON { response in
-            switch response.result {
-            case .success(_):
-                print(response)
-            case .failure:
-                print(response.error)
-            }
-        }
+//        let request =  AF.request("https://rickandmortyapi.com/api/character", method: .get)
+//        request.responseJSON { response in
+//            switch response.result {
+//            case .success(_):
+//                print(response)
+//            case .failure:
+//                print(response.error)
+//            }
+//        }
         
     }
+
     
     
 }
