43e9b9d34ce2cb603bce5c314f40b9a96c0d3dc3
Bruna Drago <bruna_drago@hotmail.com>
2021-02-02 21:06:31 -0300

    RMApp - adicionando NetworkManager

---
+++ Ricky&Morty/SupportFiles/Source/Networking/NetworkManager.swift
@@ -0,0 +1,13 @@
+//
+//  NetworkManager.swift
+//  Ricky&Morty
+//
+//  Created by Bruna Drago on 02/02/21.
+//
+
+import Foundation
+
+class NetworkManager {
+    
+    static let shared = NetworkManager()
+}
