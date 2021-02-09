492fb428d736fca8b9230c57d7a169240fe7e5f8
Bruna Drago <bruna_drago@hotmail.com>
2021-02-08 21:08:04 -0300

    RMApp creating NetworkManager and worker

--- Ricky&Morty/SupportFiles/Source/Networking/NetworkManager.swift
+++ Ricky&Morty/SupportFiles/Source/Networking/NetworkManager.swift
@@ -6,8 +6,49 @@
 //
 
 import Foundation
+import Alamofire
 
-class NetworkManager {
+typealias RequestError = (_ error: AFError?) -> Void
+typealias RequestSuccess<T: Decodable> = (_ result: T?) -> Void
+
+protocol Networking {
     
-    static let shared = NetworkManager()
+    static func request<T: Decodable>(data:RequestData,
+                                      decoder: DefaultDecoder<T>,
+                                      success: @escaping RequestSuccess<T>,
+                                      failure: @escaping RequestError)
 }
+
+class NetworkManager: Networking {
+    
+    static func request<T: Decodable>(data: RequestData,
+                                      decoder: DefaultDecoder<T>,
+                                      success: @escaping RequestSuccess<T>,
+                                      failure: @escaping RequestError) {
+        
+        let request =  AF.request(data.url, method: data.method)
+        
+        
+        request.validate().responseJSON { response in
+            switch response.result {
+            case .success(_):
+                let data = response.data ?? Data()
+                success(decoder.decode(from: data))
+            case .failure:
+                failure(response.error)
+            }
+        }
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
+
